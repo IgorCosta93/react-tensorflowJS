4401a3857fd7f6277d6af4dbfbd62bfb3945b76a
Igor Costa <igorcosta83@gmail.com>
2019-07-30 08:32:38 -0300

    remove key

--- src/components/common/Map.jsx
+++ src/components/common/Map.jsx
@@ -21,8 +21,7 @@ const MyMap = withScriptjs(withGoogleMap(Map));
 function Maps(){
     return (
         <MyMap
-            //googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyA_seX9_xLYd4qOhPjEDh_9-s4yL7fwxvE&callback=initMap"
-            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD18aXpfqMPzsvlG0F4KviWYSu2zI6_HCk"
+            //googleMapURL=""
             loadingElement={<div style={{ height: `100%` }} />}
             containerElement={<div style={{ height: `400px` }} />}
             mapElement={<div style={{ height: `100%`, width: `98%` }} />}
