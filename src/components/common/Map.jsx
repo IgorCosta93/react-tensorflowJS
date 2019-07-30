da4fbcea2b242b8f0049e9c9bf9c2708832ace9f
IgorCosta93 <igorcosta83@gmail.com>
2019-07-30 09:14:07 -0300

    Update Map.jsx

--- src/components/common/Map.jsx
+++ src/components/common/Map.jsx
@@ -22,6 +22,7 @@ function Maps(){
     return (
         <MyMap
             //googleMapURL=""
+            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
             loadingElement={<div style={{ height: `100%` }} />}
             containerElement={<div style={{ height: `400px` }} />}
             mapElement={<div style={{ height: `100%`, width: `98%` }} />}
